f2615f1df6d398a61d753e4dd58011533ca56ef9
Rob Leonard <rleonard@securityvitals.com>
2022-08-08 23:24:57 -0400

    The changepath script now updates the path and informs the user.

--- changePath.sh
+++ changePath.sh
@@ -2,5 +2,10 @@
 #A script for adding a directory to the path environment variable
 echo "What directory do you want to add to the PATH?"
 read NEWPATH
-echo "You want to add " $NEWPATH
+echo "Adding the " $NEWPATH " directory to PATH."
+PATH=$PATH:$NEWPATH
+export PATH
+echo "Your PATH environment variable is now:"
+echo $PATH
+
 exit 0
